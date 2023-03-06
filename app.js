b8bb3b0220df9b810265fb4de08ed96ff04070f0
bytechgit <103253154+bytechgit@users.noreply.github.com>
2023-03-06 17:26:31 +0100

    .

--- app.js
+++ app.js
@@ -32,6 +32,12 @@ app.use(cors({
 app.use(cors({
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
 }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
